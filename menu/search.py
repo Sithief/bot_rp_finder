800c05341f38b72fc6d130c2f7806b94f40fb3c9
Sithief <gojkljkl@gmail.com>
2019-12-21 13:52:57 +0700

    fix bugfix

--- menu/search.py
+++ menu/search.py
@@ -16,6 +16,7 @@ def get_menus():
         'choose_preset_to_search': choose_preset_to_search,
         'create_preset': create_preset,
         'delete_preset': delete_preset,
+        'change_preset': change_preset,
         # 'choose_profile_to_search': choose_profile_to_search,
         # 'search_by_profile': search_by_profile,
         'show_all_player_profiles': show_all_player_profiles,
